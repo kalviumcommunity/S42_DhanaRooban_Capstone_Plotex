5b2135ab66c053df0422ff0bc19d5843aa404f28
Dhanarooban1 <dhanazx1@gmail.com>
2024-07-18 16:05:57 +0530

    added server

--- backend/Server.js
+++ backend/Server.js
@@ -21,7 +21,6 @@ connectToMongodb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = 5000;
-
 app.listen(port, () => {
   console.log(`Server is running on Port ${port}`);
 });
