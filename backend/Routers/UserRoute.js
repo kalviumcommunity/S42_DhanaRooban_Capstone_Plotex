215ef402a2a15a59de9faf4b0f15b22aab20d461
Dhanarooban1 <dhanazx1@gmail.com>
2024-07-22 14:35:44 +0530

    done

--- backend/Routers/UserRoute.js
+++ backend/Routers/UserRoute.js
@@ -4,7 +4,7 @@ const { authenticateToken } = require("../Middleware/Authorization");
 const { Check,SignIn, getUserProfile, updateUser,LogIn } = require('../Controller/UserControl');
 
 router.get("/profile/:token",authenticateToken,getUserProfile);
-
+router.post('/sign', SignIn);
 router.put('/UpdateUser',updateUser);
 router.get('/check',Check);
 router.post('/login',LogIn)
