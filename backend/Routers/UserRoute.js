5b48049ee2b0e1af3c906e1da22a195925443b33
dhanarooban1 <dhanazx1@gmail.com>
2024-09-15 18:41:53 +0530

    done

--- backend/Routers/UserRoute.js
+++ backend/Routers/UserRoute.js
@@ -6,7 +6,11 @@ const { Check,SignIn, getUserProfile, updateUser,LogIn, GoogleAuthentication} = 
 router.get("/profile/:token",authenticateToken,getUserProfile);
 router.put('/UpdateUser',updateUser);
 router.post('/singin',SignIn)
+
+
 router.post('/gsingin',SignIn)
+
+
 router.get('/check',Check);
 router.post('/login',LogIn)
 module.exports = router;
