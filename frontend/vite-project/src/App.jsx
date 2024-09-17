1ead818ba0a1b65a4a2a7588e4dcd6faf03cfc10
dhanarooban1 <dhanazx1@gmail.com>
2024-09-17 10:36:04 +0530

    done

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Sign from './components/Sign';
+import Login from './components/Login';
+import Home from './components/Home';
+import Welcome from './Pages/Welcome';
+import AboutUs from './Pages/AboutUs';
+import FindSpace from './Pages/FindSpace';
+import RentSpace from './Pages/RentSpace';
+import BasicMap from './components/BasicMap';
+import RentForm from './components/RentForm';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/sign" element={<Sign />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/find-space" element={<FindSpace />} />
+        <Route path="/rent-space" element={<RentSpace />} />
+        <Route path="/map" element={<BasicMap />} />
+        <Route path="/" element={<Navigate to="/welcome" />} />
+        <Route path="/RentForm" element={<RentForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
