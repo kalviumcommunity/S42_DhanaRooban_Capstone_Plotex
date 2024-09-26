255ff5c677cb8b8c37a4416a65f91efdf26c13dd
R.Dhana Rooban <146908898+Dhanarooban1@users.noreply.github.com>
2024-09-26 19:51:22 +0530

    Update Home.jsx

--- frontend/vite-project/src/components/Home.jsx
+++ frontend/vite-project/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect , useState, useContext,} from "react";
+import React, { useEffect, useState, useContext } from "react";
 import {
   Box,
   Center,
@@ -9,52 +9,47 @@ import {
 import UserContext from "../Atoms/UserContext";
 import { useNavigate } from "react-router-dom";
 import { homePageContent } from "../Services/FrontendData";
-import AboutUs from "../Pages/AboutUs"
+import AboutUs from "../Pages/AboutUs";
+
 function Home() {
   const [userName, setUserName] = useState('');
   const { currentUser, loading } = useContext(UserContext);
 
-
-  return (
-
-  const navigate = useNavigate();
   useEffect(() => {
     if (currentUser && currentUser.email) {
-      const firstFourLetters = currentUser.email.substring(0,5).toLowerCase();
+      const firstFourLetters = currentUser.email.substring(0, 5).toLowerCase();
       setUserName(firstFourLetters);
     }
   }, [currentUser]);
-  
+
   if (loading) {
     return <div>Loading...</div>;
   }
-  
+
   const handleAccessHome = () => {
     navigate('/find-space');
   };
-  return (  
 
+  return (
     <>
-    <Center h="100vh" overflowY="auto"
-    p={4} >
-      <VStack spacing={4}>
-        <Text className="Bold" fontSize="2xl">
-          Welcome { userName || 'Guest!'}
-        </Text>
-        <Button
-       onClick={handleAccessHome}
-          colorScheme="green"
-          size="lg"
-          className="Semibold"
-        >
-          ACCESS NAME HOME
-        </Button>
-      </VStack>
-    </Center>
-    <AboutUs/>
+      <Center h="100vh" overflowY="auto" p={4}>
+        <VStack spacing={4}>
+          <Text className="Bold" fontSize="2xl">
+            Welcome {userName || 'Guest!'}
+          </Text>
+          <Button
+            onClick={handleAccessHome}
+            colorScheme="green"
+            size="lg"
+            className="Semibold"
+          >
+            ACCESS NAME HOME
+          </Button>
+        </VStack>
+      </Center>
+      <AboutUs />
     </>
   );
 }
 
 export default Home;
-
