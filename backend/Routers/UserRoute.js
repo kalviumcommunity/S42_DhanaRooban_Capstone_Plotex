c8427fa2af9b9e2c1236c3b56668a4cdd07fbd22
aaryan <aaryanpanda04@gmail.com>
2024-09-30 23:44:41 +0530

    Implemented rate limiting using express-rate-limit

--- backend/Routers/UserRoute.js
+++ backend/Routers/UserRoute.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { authenticateToken } = require("../Middleware/Authorization");
 const { Check,SignIn, getUserProfile, updateUser,LogIn, GoogleAuthentication} = require('../Controller/UserControl');
+import apiLimiter from '../Middleware/RateLimiter';
 
 router.get("/profile/:token",authenticateToken,getUserProfile);
 router.put('/UpdateUser',updateUser);
 
-router.post('/sign',SignIn)
-router.post('/singin',SignIn)
-router.post('/gsingin',SignIn)
-router.get('/check',Check);
-router.post('/login',LogIn)
+router.post('/sign',apiLimiter,SignIn)
+router.post('/singin',apiLimiter, SignIn)
+router.post('/gsingin',apiLimiter ,SignIn)
+router.get('/check',apiLimiter, Check);
+router.post('/login',apiLimiter,LogIn)
 module.exports = router;
